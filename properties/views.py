c51e40fab31f4b156afed7cc068d3b191dd361c4
E-Commerce-Project <hashusk786@gmail.com>
2026-05-04 22:57:01 +0530

    property api fix

--- properties/views.py
+++ properties/views.py
@@ -1,25 +1,13 @@
-from django.shortcuts import render
-
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import viewsets
-
-from .models import *
-
-from .serializers import PropertySerializer
-from .serializers import InquirySerializer
-
-
+from .models import Property, Inquiry
+from .serializers import PropertySerializer, InquirySerializer
 
 
 class PropertyViewSet(ModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
 
-    def get_serializer_context(self):
-        return {'request': self.request}
-    
-    
 
-class InquiryViewSet(viewsets.ModelViewSet):
+class InquiryViewSet(ModelViewSet):
     queryset = Inquiry.objects.all()
     serializer_class = InquirySerializer
