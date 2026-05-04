7a2dafa346e5a96021eb3cbb07b208ae13fa656d
E-Commerce-Project <hashusk786@gmail.com>
2026-05-04 22:52:23 +0530

    fix image and serializer

--- properties/serializers.py
+++ properties/serializers.py
@@ -1,33 +1,11 @@
-# Converts data → JSON
-# JSON → used by frontend
-# Serializer =
-# “Take data from database → convert into JSON”
-
 from rest_framework import serializers
-from .models import *
+from .models import Property, Inquiry
 
 
 class PropertySerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-
     class Meta:
         model = Property
         fields = '__all__'
-
-    def get_image(self, obj):
-        try:
-            if obj.image:
-                request = self.context.get('request')
-
-                if request:
-                    return request.build_absolute_uri(obj.image.url)
-
-                return obj.image.url
-
-            return None
-
-        except:
-            return None
 
 
 class InquirySerializer(serializers.ModelSerializer):
