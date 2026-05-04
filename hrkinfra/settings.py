954c8faa22386ca543acf5be3737a59f894fdc33
E-Commerce-Project <hashusk786@gmail.com>
2026-05-04 21:15:59 +0530

    Prepare Infra for Render deployment

--- hrkinfra/settings.py
+++ hrkinfra/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
+DEBUG = os.environ.get("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["*"]
+
+ALLOWED_HOSTS = ["hrkinfra.onrender.com"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
