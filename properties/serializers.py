e31e5f8d81e950f519e39a5e83e0edcaf9daa749
E-Commerce-Project <hashusk786@gmail.com>
2026-05-05 21:07:33 +0530

    fixed 500 again 2

--- properties/serializers.py
+++ properties/serializers.py
@@ -3,14 +3,10 @@ from .models import Property, Inquiry
 
 
 class PropertySerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-
     class Meta:
         model = Property
         fields = '__all__'
 
-    def get_image(self, obj):
-        return None
 
 
 class InquirySerializer(serializers.ModelSerializer):
